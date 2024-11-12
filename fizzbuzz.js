7f2dbb6b1a9ba7d0162afc21394f8bb592e097dd
naimdeb <naimdebard02@gmail.com>
2024-11-12 13:41:51 +0100

    add multiplae solutions

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,20 +1,44 @@
 function fizzbuzz(nombre) {
-  if (nombre % 3 == 0 && nombre % 5 !== 0) {
-    console.log("fizz");
-  }
-  if (nombre % 5 == 0 && nombre % 3 !== 0) {
-    console.log("buzz");
-  }
-  if (nombre % 5 == 0 && nombre % 3 == 0) {
-    console.log("fizzbuzz");
-  }
-  if (nombre % 5 !== 0 && nombre % 3 !== 0) {
-    console.log(nombre);
-  }
-}
 
-fizzbuzz(2);
-fizzbuzz(5);
-fizzbuzz(6);
-fizzbuzz(11);
-fizzbuzz(30);
+
+
+    // EN 4 IF
+        //   if (nombre % 3 == 0 && nombre % 5 !== 0) {
+        //     console.log("fizz");
+        //   }
+        //   if (nombre % 5 == 0 && nombre % 3 !== 0) {
+        //     console.log("buzz");
+        //   }
+        //   if (nombre % 5 == 0 && nombre % 3 == 0) {
+        //     console.log("fizzbuzz");
+        //   }
+        //   if (nombre % 5 !== 0 && nombre % 3 !== 0) {
+        //     console.log(nombre);
+        //   }
+    
+    let texte ='';    
+    // EN 3 IF
+        // if (nombre % 3 == 0)
+        //     {texte +="fizz"}
+    
+        // if (nombre % 5 == 0)
+        //     {texte+="buzz"}
+    
+        // if (nombre % 3 !== 0 && nombre % 5 !== 0){texte+=nombre}
+    
+        // console.log(texte)
+    
+    
+
+    // En 2 if
+
+    
+// if (nombre % 3 == 0)
+//     texte+="fizz"
+
+// if (nombre % 5 == 0)
+//     texte+="buzz"
+
+// console.log(texte || nombre)
+
+    
